ff2594eba50fbefca036cfcca8c54615c604ae2b
schkiho <schkiho@yahoo.de>
2020-07-15 20:01:47 +0200

    html template setup

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+// setup materialize components
+document.addEventListener("DOMContentLoaded", function () {
+  let modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
+
+  let items = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(items);
+});
