d2a64bd1bfa8893fb488a14ab504c8c6142e8fa1
schkiho <schkiho@yahoo.de>
2020-07-17 13:45:31 +0200

    logging user in

--- scripts/auth.js
+++ scripts/auth.js
@@ -24,3 +24,22 @@ logout.addEventListener("click", (e) => {
     console.log("user signed out");
   });
 });
+
+// login user
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  // login user
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the login modal and reset form
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
