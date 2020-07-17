d8d0322f39687b504bd7116f7cb0233907fdec62
schkiho <schkiho@yahoo.de>
2020-07-17 13:56:26 +0200

    tracking auth status

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,12 @@
+// listen for auth status changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log("user logged in", user);
+  } else {
+    console.log("user logged out");
+  }
+});
+
 // signup
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
@@ -9,7 +18,6 @@ signupForm.addEventListener("submit", (e) => {
 
   // signup user
   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
     const modal = document.querySelector("#modal-signup");
     M.Modal.getInstance(modal).close();
     signupForm.reset();
@@ -20,9 +28,7 @@ signupForm.addEventListener("submit", (e) => {
 const logout = document.querySelector("#logout");
 logout.addEventListener("click", (e) => {
   e.preventDefault();
-  auth.signOut().then(() => {
-    console.log("user signed out");
-  });
+  auth.signOut();
 });
 
 // login user
@@ -36,7 +42,6 @@ loginForm.addEventListener("submit", (e) => {
 
   // login user
   auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
     // close the login modal and reset form
     const modal = document.querySelector("#modal-login");
     M.Modal.getInstance(modal).close();
